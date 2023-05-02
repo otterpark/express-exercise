284c3ed4acd47e0f08216f2587da95b5fb433894
박진우 <otterpark94@gmail.com>
2023-05-03 00:24:20 +0900

    Feat: CORS 추가 및 origin 설정(localhost React 기본 포트 설정)

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
 import express, {type Request, type Response} from 'express';
+import cors from 'cors';
+
 import api from './api';
 
 const port = 3000;
 
 const app = express();
+
+// CORS origin 설정
+app.use(cors({
+	origin: [
+		'http://localhost:8080',
+	],
+}));
 
 // Request Body를 express에서 json으로 받아온다.
 app.use(express.json());
